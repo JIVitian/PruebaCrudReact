8eae80982678b685e5c9f9c3c23e58241ac433be
JIVitian <jivitian@gmail.com>
2021-10-28 23:37:02 -0300

    Created temporal DB

---
+++ src/components/CrudApp.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const initialDB = [
+  {
+    id: 1,
+    name: "Seiya",
+    constellation: "Pegaso",
+  },
+  {
+    id: 2,
+    name: "Shiryu",
+    constellation: "Dragon",
+  },
+  {
+    id: 3,
+    name: "Hyoga",
+    constellation: "Cisne",
+  },
+  {
+    id: 4,
+    name: "Shun",
+    constellation: "Andrómeda",
+  },
+  {
+    id: 5,
+    name: "Ikki",
+    constellation: "Fénix",
+  },
+];
+
+export default function CrudApp() {
+  return (
+    <div>
+      <h2>CRUD App</h2>
+    </div>
+  );
+}
