cdcf1eed42ea2a839b47c44fd9ebbb7a35e92e4e
JIVitian <jivitian@gmail.com>
2021-11-01 23:04:10 -0300

    Loader funcionando

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -2,20 +2,35 @@ import React, { useState, useEffect } from "react";
 import { helpHttp } from "../helpers/helpHttp";
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
+import Loader from "./Loader";
+import Message from "./Message";
 
 export default function CrudApi() {
-  const [db, setDb] = useState([]);
+  const [db, setDb] = useState(null);
   const [dataToEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const api = helpHttp();
   let url = "http://localhost:5000/santos";
 
   useEffect(() => {
+    setLoading(true);
+
     api.get(url).then((res) => {
       console.log(res);
-      setDb(!res.err ? res : null);
+      if (!res.err) {
+        setDb(res);
+        setError(null);
+      } else {
+        setDb(null);
+        setError(res.err);
+      }
+
+      setLoading(false);
     });
   }, []);
+
   // Crea un nuevo registro en la "Base de datos"
   const createData = (data) => {
     data.id = Date.now();
@@ -50,11 +65,15 @@ export default function CrudApi() {
           dataToEdit={dataToEdit}
           setDataToEdit={setDataToEdit}
         />
-        <CrudTable
-          data={db}
-          setDataToEdit={setDataToEdit}
-          deleteData={deleteData}
-        />
+        {loading && <Loader />}
+        {error && <Message />}
+        {db && (
+          <CrudTable
+            data={db}
+            setDataToEdit={setDataToEdit}
+            deleteData={deleteData}
+          />
+        )}
       </article>
     </div>
   );
---
+++ src/components/Message.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Si hay un mensaje de error, lo manejo mediante este componente
+export default function Message() {
+  return (
+    <div>
+      <h2>Message</h2>
+    </div>
+  );
+}
